ab17675edf72fc3c60adeeacfbcde89147cecaab
nrtyc4 <nrtyc4@mail.missouri.edu>
2014-01-31 22:21:52 -0600

    Basic Go programs implementing channels and goroutines

---
+++ goProjects/goroutines/goroutines.go
@@ -0,0 +1,29 @@
+/*
+	Open up 10 goroutines and run them simultaneously!
+	**The scanln is at the bottom to keep the program from
+		exiting before the goroutines can finish** 
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+func f(n int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(n, ":", i)
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
+	}
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go f(i)
+	}
+	var input string
+	fmt.Scanln(&input)
+}
