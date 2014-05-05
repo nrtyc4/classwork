bccb342486aa256a19a8cc4e03bc4bb727e3d4ee
nrtyc4 <natthompson72@gmail.com>
2014-05-05 02:19:23 -0500

    Added error handling

--- searchify/searchify.go
+++ searchify/searchify.go
@@ -10,14 +10,16 @@ import (
 
 func artistHandler(w http.ResponseWriter, req *http.Request) {
 
-	//fmt.Println(req)
+	fmt.Println(req)
 	fmt.Println(req.RequestURI)
 
 	artisturl := []string{"https://ws.spotify.com/search/1"}
 	appendedURL := strings.Join(artisturl, req.RequestURI)
 
-	res, _ := http.Get(appendedURL)
-	buf, _ := json.Marshal(res)
+	res, err := http.Get(appendedURL)
+	if err != nil { log.Fatal( fmt.Println("Couldn't append URL")) }
+	buf, err := json.Marshal(res)
+	if err != nil { log.Fatal( fmt.Println("Couldn't marshal JSON")) }
 	w.Write(buf)
 }
 
