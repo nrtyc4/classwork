d3ca3f1cb120cec177f976561e9ac0a72dce1007
nrtyc4 <nrtyc4@mail.missouri.edu>
2014-05-04 20:38:56 -0500

    A few handler updates on the searchify server

--- searchify/searchify.go
+++ searchify/searchify.go
@@ -1,9 +1,24 @@
 package main
 
-import "net/http"
+import (
+  "net/http"
+  "log"
+)
+
+func artistHandler(w http.ResponseWriter, req *http.Request) {
+  switch req.Method {
+  case "GET":
+    buf, err := json.Marshal(someMapofStrings)
+    w.write(buf)
+  default:
+    w.WriteHeader(400)
+  }
+}
 
 func main(){
-	http.Handle("/", http.FileServer( http.Dir("./") )
+	http.Handle("/", http.FileServer(http.Dir("./")) )
 
-	http.ListenAndServe(":8000", nil)
+  http.HandleFunc("/artist", artistHandler)
+  
+	log.fatal(http.ListenAndServe(":8080", nil))
 }
