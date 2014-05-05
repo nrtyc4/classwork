b7e0e62281723f71b139b4bfc55d5a2fcebb1a99
nrtyc4 <natthompson72@gmail.com>
2014-05-05 16:44:26 -0500

    Modified searchify.go

--- searchify/searchify.go
+++ searchify/searchify.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-  "net/http"
-  "log"
+	"log"
+	"net/http"
 )
 
 func artistHandler(w http.ResponseWriter, req *http.Request) {
-  switch req.Method {
-  case "GET":
-    buf, err := json.Marshal(someMapofStrings)
-    w.write(buf)
-  default:
-    w.WriteHeader(400)
-  }
+	switch req.Method {
+	case "GET":
+		buf, err := json.Marshal(someMapofStrings)
+		w.write(buf)
+	default:
+		w.WriteHeader(400)
+	}
 }
 
-func main(){
-	http.Handle("/", http.FileServer(http.Dir("./")) )
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./")))
 
-  http.HandleFunc("/artist", artistHandler)
-  
+	http.HandleFunc("/artist", artistHandler)
+
 	log.fatal(http.ListenAndServe(":8080", nil))
 }
