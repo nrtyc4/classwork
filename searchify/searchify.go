35f7b232af913a7c7192cbdb3e4923707d6a56db
nrtyc4 <nrtyc4@mail.missouri.edu>
2014-05-05 06:54:16 +0000

    New updates for searchify back-end.
    Still trying to understand how go approaches http handling.

--- searchify/searchify.go
+++ searchify/searchify.go
@@ -1,9 +1,37 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt" //debugging purposes for now
+	"log"
+	"net/http"
+	"strings"
+)
 
-func main(){
-	http.Handle("/", http.FileServer( http.Dir("./") )
+func artistHandler(w http.ResponseWriter, req *http.Request) {
+	/* switch req.Method {
+	case "GET":
+	  buf, err := json.Marshal(someMapofStrings)
+	  w.Write(buf)
+	default:
+	  w.WriteHeader(400)
+	} */
 
-	http.ListenAndServe(":8000", nil)
+	//fmt.Println(req)
+	fmt.Println(req.RequestURI)
+
+	artisturl := []string{"https://ws.spotify.com/search/1"}
+	appendedURL := strings.Join(artisturl, req.RequestURI)
+
+	res, _ := http.Get(appendedURL)
+	buf, _ := json.Marshal(res)
+	w.Write(buf)
+}
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./")))
+
+	http.HandleFunc("/artist.json", artistHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
